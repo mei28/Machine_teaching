35a6dad00bca63121c7c0911d35aa40502b6f6c9
楊明哲 <mingzhe.yang@hcomp.cs.tsukuba.ac.jp>
2019-12-05 22:15:16 +0900

    [add] implement estimate min_w

---
+++ src/oracle.py
@@ -0,0 +1,36 @@
+from model import *
+import import_path
+import pandas as pd
+import numpy as np
+import theano
+import theano.tensor as T
+from collections import OrderedDict
+# theano.config.gcc.cxxflags = "-Wno-c++11-narrowing"
+
+
+class Oracle():
+    min_w = None
+
+    def __init__(self, eta=0.01, lambd=0.01):
+        """[summary]
+
+        Parameters
+        ----------
+        eta : float, optional
+            min_w's parameter, by default 0.01
+        lambd : float, optional
+            W's parameter, by default 0.01
+        """
+        self.eta = eta
+        self.lambd = lambd
+
+    def estimate_min_w(self, X, y, training_epochs=10):
+        w_init = np.random.normal(
+            loc=0,
+            scale=self.eta,
+            size=X.shape[1]
+        )
+        logistic_model = Logistic_model(w_init, self.eta)
+        logistic_model.learn(X, y, training_epochs)
+        self.min_w = logistic_model.w
+        return self.min_w
