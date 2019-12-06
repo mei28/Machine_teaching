898adda456f47cf8a877c04817593955bde32f02
楊明哲 <mingzhe.yang@hcomp.cs.tsukuba.ac.jp>
2019-12-06 13:50:54 +0900

    [add] make W init

--- src/oracle.py
+++ src/oracle.py
@@ -34,3 +34,26 @@ class Oracle():
         logistic_model.learn(X, y, training_epochs)
         self.min_w = logistic_model.w
         return self.min_w
+
+    def make_W_init(self, J):
+        """make first W
+
+        Parameters
+        ----------
+        J : int
+            the number of worker
+
+        Returns
+        -------
+        return W numpy
+            shape = (J,D)
+        """
+        W = np.zeros((J, self.min_w.shape[0]))
+
+        for j in range(J):
+            W[j, :] = np.random.normal(
+                loc=self.min_w,
+                scale=self.lambd,
+                size=self.min_w.shape[0]
+            )
+        return W
