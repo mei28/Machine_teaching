fde15685ef288c4b1b4eb42940cf85829cf4b4ae
楊明哲 <mingzhe.yang@hcomp.cs.tsukuba.ac.jp>
2019-12-08 12:05:32 +0900

    [add] implement omniscient teacher

---
+++ src/omniscient_teacher.py
@@ -0,0 +1,51 @@
+import import_path
+import numpy as np
+import pandas as pd
+from model import *
+from oracle import Oracle
+from teacher import Teacher
+
+
+class Omniscient(Teacher):
+    def __init__(self, min_w, eta=0.01):
+        super().__init__(min_w, eta=eta)
+
+    def make_loss_function(self):
+        """
+        return omniscient teacher function
+
+        Parameters
+        ----------
+        eta : float, optional
+            w_t parameter, by default 0.01
+
+        Returns
+        -------
+        return function
+            inputs = [grad_loss,w_t,w_]
+            outputs = [loss]
+        """
+        grad_loss = T.matrix(name='grad_loss')
+        w_ = T.vector(name='w_')
+        w_t = T.vector(name='w_t')
+
+        first = (self.eta ** 2) * (grad_loss ** 2).sum(axis=1)
+        second = -2 * self.eta * (T.dot(grad_loss, w_t - w_))
+        loss = first + second
+        function = theano.function(
+            inputs=[grad_loss, w_t, w_],
+            outputs=[loss],
+            on_unused_input='ignore'
+        )
+        return function
+
+    def return_textbook(self, X, y, w_t, w_):
+        grad_loss = self.make_grad_loss_matrix(X, y, w_t)
+        choicer = self.make_loss_function()
+        loss_matrix = choicer(grad_loss, w_t, w_)
+        index = self.return_argmin_index(loss_matrix)
+        print('omni: {}'.format(index))
+        X_t, y_t = X.iloc[index], y.iloc[index]
+        self.drop_textbook(X, y, index)
+
+        return X_t, y_t, index
