ebafc8ea4c969d165cd693e9a32e6223b99d7b86
楊明哲 <mingzhe.yang@hcomp.cs.tsukuba.ac.jp>
2019-12-08 13:37:38 +0900

    [add] implement surrogate teacher

---
+++ src/surrogate_teacher.py
@@ -0,0 +1,38 @@
+from teacher import Teacher
+import numpy as np
+import pandas as pd
+import theano
+import theano.tensor as T
+
+
+class Surrogate(Teacher):
+    def __init__(self, min_w, eta=0.01):
+        super().__init__(min_w, eta=eta)
+
+    def make_loss_function(self):
+        grad_loss_ = T.matrix(name='grad_loss')
+        loss_t = T.vector(name='loss_t')
+        loss__ = T.vector(name='loss__')
+        first = (self.eta**2)*(grad_loss_ ** 2).sum()
+        second = -2*self.eta*(loss_t-loss__)
+        loss = first + second
+        t = theano.function(
+            inputs=[grad_loss_, loss_t, loss__],
+            outputs=[loss],
+        )
+        return t
+
+    def return_textbook(self, X, y, w_t, w_):
+        grad_loss = self.make_grad_loss_matrix(X, y, w_t)
+        loss = self.loss_function()
+        loss_t = np.array(loss(X, y, w_t)).flatten()
+        loss__ = np.array(loss(X, y, w_)).flatten()
+
+        choicer = self.make_loss_function()
+        loss_matrix = choicer(grad_loss, loss_t, loss__)
+        index = self.return_argmin_index(loss_matrix)
+        print('surr: {}'.format(index))
+        X_t, y_t = X.iloc[index], y.iloc[index]
+        self.drop_textbook(X, y, index)
+
+        return X_t, y_t, index
