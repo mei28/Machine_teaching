5641f3ef23f64b2f596571c5f50aebcf9bbee955
楊明哲 <mingzhe.yang@hcomp.cs.tsukuba.ac.jp>
2020-05-15 13:11:52 +0900

    [upgrade] runpy

--- run.py
+++ run.py
@@ -11,6 +11,8 @@ from utils import predict, predict_by_W, rmse_W, write_np2csv, rmse_w, make_rand
 from load_data import read_W, read_csv, split_data
 from tqdm import tqdm
 from sklearn.metrics import roc_auc_score
+import logging
+import datetime
 # %%
 df = read_csv('output/weebil_vespula_pm1.csv', header=0)
 train_X, test_X, train_y, test_y = split_data(df, False)
@@ -18,11 +20,11 @@ eta, lambd, alpha = 1, 2, 0.01
 training_epochs, loops = 10, 10
 J = 10
 # 提示する教材合計数
-textbook = 501
+textbook = 2
 # 推定に使う教材数
-test_textbook_list = [10, 100, 1000]
+test_textbook_list = [100]
 # 推定間に提示する教材数
-between_textbook_list = [1, 10, 100]
+between_textbook_list = [1]
 # 組
 k = 1
 
@@ -35,13 +37,22 @@ W = W_init.copy()
 train_X_ = train_X.copy()
 train_y_ = train_y.copy()
 
-result_path = 'result/202005515/'
-
+now = datetime.datetime.now()
+now_str = now.strftime('%Y%m%d%H%M')
+result_path = 'result/{}_{}'.format(now_str, k)
+logging.basicConfig(
+    filename='./logs/log_{0:%Y%m%d%H%M%S}_{1}.log'.format(now, k), level=logging.DEBUG
+)
+logging.debug('./logs/log_{0:%Y%m%d%H%M%S}_{1}.log'.format(now, k))
+logging.debug('min_w')
+logging.debug(min_w)
+logging.debug('eta,lambd')
+logging.debug([eta, lambd])
 # %%
 
 # %%
 # Omniscient
-
+logging.debug('Omniscient')
 train_X_ = train_X.copy()
 train_y_ = train_y.copy()
 W = W_init.copy()
@@ -53,12 +64,11 @@ for i in range(textbook):
 
     print("{}: {}".format(i, predict_by_W(test_X, test_y, omt.W)))
     omt.show_textbook(X=train_X_, y=None, N=1, option='min_w')
-
+    logging.debug(predict_by_W(test_X, test_y, omt.W))
 a = a[1:]
 write_np2csv(
-    a, '{}Omniscient/omni-{}.csv'.format(result_path, k))
+    a, '{}_{}.csv'.format(result_path, 'omniscient'))
 print('{}: finished.'.format(k))
-
 # %%
 # Random
 
@@ -67,22 +77,23 @@ train_y_ = train_y.copy()
 W = W_init.copy()
 
 rat = Random(min_w, W, N=train_X_.shape[0], alpha=alpha)
-
+logging.debug('Random')
 a = np.zeros(1)
 for i in range(textbook):
     a = np.vstack((a, predict_by_W(test_X, test_y, rat.W)))
 
     print("{}: {}".format(i, predict_by_W(test_X, test_y, rat.W)))
     rat.show_textbook(train_X_, y=None, N=1, option='min_w')
-
+    logging.debug(predict_by_W(test_X, test_y, rat.W))
 a = a[1:]
-write_np2csv(a, '{}Random/ran-{}.csv'.format(result_path, k))
+write_np2csv(a, '{}_{}.csv'.format(result_path, 'random'))
 print('{}: finished.'.format(k))
 
 # %%
 for t_num in test_textbook_list:
     for b_num in between_textbook_list:
         # wot
+        logging.debug('without teacher')
         w_init = np.random.normal(loc=0, scale=lambd, size=min_w.shape)
         W = W_init.copy()
         train_X_ = train_X.copy()
@@ -110,10 +121,12 @@ for t_num in test_textbook_list:
             wot.show_textbook(train_X_, y=None, N=1, option='w_star')
             print(predict(test_X, test_y, min_w), roc_auc_score(
                 test_y, wot.predict_y(test_X, wot.w_star)))
+            logging.debug(predict_by_W(test_X, test_y, wot.W))
+            logging.debug(predict_by_W(test_X, test_y, wot.W_star))
 
         a = a[1:]
         write_np2csv(
-            a, '{}Without_teacher/w_star/w-t{}b{}-{}.csv'.format(result_path, t_num, b_num, k))
+            a, '{}_{}_{}_{}.csv'.format(result_path, "wot", t_num, b_num))
 
         print('t{}b{}: finished.'.format(t_num, b_num))
 
